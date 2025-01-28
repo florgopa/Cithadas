69f9de6a0bb341f35995a8bfe4d1e7515a3b2b04
florgopa <florgomezpacheco@gmail.com>
2025-01-28 20:43:32 -0300

    primera prueba

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>¡Hola, mundo en PHP!</h1>
+    <p>Esta en PHP</p>
+    
+    <?php
+        echo "Este mensaje está generado con PHP.";
+    ?>
+</body>
+</html>
